4377b9176099824bbda079b9f6dbcb2f3ad4cfd3
Joris Jansen <dev.github@tpotjj.com>
2024-12-21 20:36:51 +0100

    Add falling poem text

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const snowContainer = document.querySelector(".snow-container");
+    const poemContainer = document.querySelector(".poem-container");
 
     const particlesPerThousandPixels = 0.1;
     const fallSpeed = 0.75;
@@ -7,17 +8,49 @@ document.addEventListener("DOMContentLoaded", function () {
     const maxSnowflakes = 200;
     const snowflakes = [];
 
+    const poemLines = [
+        "Aller liefste Suus,",
+        "De papieren gedichten zullen al snel vergaan,",
+        "maar dit technisch ingestoken stukje poetisch epos, blijft langer bestaan.",
+        "Een vleugje attentie van jou is hierbij onontbeerlijk,",
+        "geen zorgen, dit kan met een glas wijn vanuit je luie stoel, heerlijk.",
+        "Zie hoe op het scherm, iconen neerdwarrelen als in een winters tafereel,",
+        "Klik op de iconen en genereer het gedicht manueel.",
+    ];
+
+    let currentLine = 0;
+
+    function revealNextLine() {
+        if (currentLine < poemLines.length) {
+            const lineElement = document.createElement("div");
+            lineElement.textContent = poemLines[currentLine];
+            lineElement.classList.add("falling-line");
+            poemContainer.appendChild(lineElement);
+
+            lineElement.style.left = `${Math.random() * 80}vw`;
+            currentLine++;
+        }
+    }
+
+    let poemInterval = setInterval(() => {
+        if (currentLine < poemLines.length) {
+            requestAnimationFrame(revealNextLine);
+        } else {
+            clearInterval(poemInterval);
+        }
+    }, 3000);
+
     let snowflakeInterval;
     let isTabActive = true;
 
     function resetSnowflake(snowflake) {
         const size = Math.random() * 5 + 1;
-        const viewportWidth = window.innerWidth - size; // Adjust for snowflake size
+        const viewportWidth = window.innerWidth - size;
         const viewportHeight = window.innerHeight;
 
         snowflake.style.width = `${size}px`;
         snowflake.style.height = `${size}px`;
-        snowflake.style.left = `${Math.random() * viewportWidth}px`; // Constrain within viewport width
+        snowflake.style.left = `${Math.random() * viewportWidth}px`;
         snowflake.style.top = `-${size}px`;
 
         const animationDuration = (Math.random() * 3 + 2) / fallSpeed;
@@ -30,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (parseInt(snowflake.style.top, 10) < viewportHeight) {
                 resetSnowflake(snowflake);
             } else {
-                snowflake.remove(); // Remove when it goes off the bottom edge
+                snowflake.remove();
             }
         }, animationDuration * 1000);
     }
